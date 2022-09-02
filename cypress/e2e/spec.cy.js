dcb968a04ccd0d7338523c84b2ea50797cf8e284
bharath-kumar-cko <bharath.sadanandam@checkout.com>
2022-09-02 14:17:26 +0300

    yaml

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,7 +4,6 @@ describe('empty spec', () => {
   it('passes', () => {
     cy.once('uncaught:exception', () => false);
     cy.visit('http://localhost:3000');
-    cy.once('uncaught:exception', () => false);
     cy.contains('div', 'Hello!!!').should('be.visible');
   })
 })
